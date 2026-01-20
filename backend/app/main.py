4643add67eb40f421e624e370cb07c72102c20ea
nfarkas217 <78665654+nfarkas217@users.noreply.github.com>
2026-01-19 21:02:46 -0500

    remove CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,14 +25,6 @@ df = df[["SubGroup", "Category", "Students", "Enrollment", "PctEnrollment", "Inc
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.get("/api/data")
 def get_data(category: str):
     # Filter by category
